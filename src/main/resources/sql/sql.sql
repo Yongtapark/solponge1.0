b765a0f9340c2156acadc87f2a799e421f41bd18
leyel96 <ttt6774@naver.com>
2023-02-27 16:44:48 +0900

    sql문 추가(예인)

--- src/main/resources/sql/sql.sql
+++ src/main/resources/sql/sql.sql
@@ -30,3 +30,43 @@ CREATE TABLE book (
                       book_publisher VARCHAR2(50)
 );
 
+
+CREATE SEQUENCE product_SEQ
+    START WITH 1
+    INCREMENT BY 1;
+
+
+CREATE TABLE product (
+                         product_num NUMBER PRIMARY KEY,
+                         product_title VARCHAR2(1000),
+                         product_subtitle VARCHAR2(1000),
+                         product_writer VARCHAR2(1000),
+                         product_price VARCHAR2(1000),
+                         product_img VARCHAR2(4000),
+                         product_date DATE,
+                         product_page VARCHAR2(1000),
+                         product_code CLOB,
+                         product_stock NUMBER default 10,
+                         product_sale NUMBER default 1,
+                         product_visit NUMBER default 1
+);
+
+CREATE SEQUENCE job_info_SEQ
+    START WITH 1
+    INCREMENT BY 1;
+
+
+CREATE TABLE job_info(
+                         job_info_num NUMBER PRIMARY KEY,
+                         job_info_Field VARCHAR2(1000),
+                         job_info_company_name VARCHAR2(1000),
+                         job_info_posting_name VARCHAR2(1000),
+                         job_info_field_tag_list VARCHAR2(1000),
+                         job_info_Qualification VARCHAR2(1000),
+                         job_info_work_type VARCHAR2(1000),
+                         job_info_work_area VARCHAR2(1000),
+                         job_info_Deadline VARCHAR2(1000),
+                         job_info_page_code CLOB,
+                         job_info_link VARCHAR2(4000)
+);
+
